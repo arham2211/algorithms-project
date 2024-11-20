46e22aaeabb8319dcba22dfea687403121293425
Asim Ali <m.asimali100@gmail.com>
2024-11-20 23:49:23 +0500

    final change

--- generate_for_cpp.py
+++ generate_for_cpp.py
@@ -1,18 +1,19 @@
 
 import random
 
-i=1
 
-while(i<=10):
-    vals = [(random.randint(-99, 99), random.randint(-99, 99)) for _ in range(20)]
-    file_name = f"./cpp_inputs/cpp_input_{i}.txt"
+def run():
+    i=1
+    while(i<=10):
+        vals = [(random.randint(-99, 99), random.randint(-99, 99)) for _ in range(20)]
+        file_name = f"./cpp_inputs/cpp_input_{i}.txt"
 
 
-    # Open the file in write mode (this will create the file if it doesn't exist)
-    with open(file_name, "w") as file:
-            for x,y in vals:
-                file.write(f"{x} {y} \n")
+        # Open the file in write mode (this will create the file if it doesn't exist)
+        with open(file_name, "w") as file:
+                for x,y in vals:
+                    file.write(f"{x} {y} \n")
 
-    i+=1            
+        i+=1            
 
-print(f"File '{file_name}' has been created and written to.")
+    print(f"File '{file_name}' has been created and written to.")
--- generate_for_karatsuba.py
+++ generate_for_karatsuba.py
@@ -1,18 +1,21 @@
 
 import random
 
-i=1
+def run():
+    i=1
 
-while(i<=10):
-    vals = [(random.randint(10, 100), random.randint(10, 100)) for _ in range(500)]
-    file_name = f"./karatsuba_inputs/karatsuba_input_{i}.txt"
+    while(i<=10):
+        vals = [(random.randint(10, 100000000000000000000), random.randint(10, 100000000000000000)) for _ in range(500)]
+        file_name = f"./karatsuba_inputs/karatsuba_input_{i}.txt"
 
 
-    # Open the file in write mode (this will create the file if it doesn't exist)
-    with open(file_name, "w") as file:
-            for x,y in vals:
-                file.write(f"{x} {y} \n")
+        # Open the file in write mode (this will create the file if it doesn't exist)
+        with open(file_name, "w") as file:
+                for x,y in vals:
+                    file.write(f"{x} {y} \n")
 
-    i+=1            
+        i+=1            
 
-print(f"File '{file_name}' has been created and written to.")
+    print(f"File '{file_name}' has been created and written to.")
+
+#run()
